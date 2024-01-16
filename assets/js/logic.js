c4fa891497324a049a12a5c6a0045f45ee636ed2
Volodymyr Shtyka <vladimirshtyka@gmail.com>
2024-01-16 20:57:50 +0000

    Add event listeners for buttons

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -14,3 +14,24 @@ let currentQuestionIndex = 0;
 let countdownTimer;
 let timeLeft = 75; // Initial time for the quiz
 let userScore = 0;
+
+// Event listener for the start button
+startButton.addEventListener("click", beginQuiz);
+
+// Event listener for the choices container
+choicesContainer.addEventListener("click", handleChoiceSelection);
+
+// Event listener for the submit button
+submitButton.addEventListener("click", recordUserScore);
+
+function beginQuiz() {
+    
+}
+
+function handleChoiceSelection() {
+    
+}
+
+function recordUserScore() {
+    
+}
